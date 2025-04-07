4c837d25a824f07eaee03471bdd311249afadf34
anetaelcknerova <aneta.elcknerova@gmail.com>
2025-04-07 09:19:47 +0200

    hotovo

--- searching.py
+++ searching.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 # get current working directory path
 cwd_path = os.getcwd()
@@ -11,12 +12,34 @@ def read_data(file_name, field):
     :param field: (str), field of a dict to return
     :return: (list, string),
     """
-    file_path = os.path.join(cwd_path, file_name)
+    with open(file_name, "r") as file_obj:
+        data = json.load(file_obj)
+        if field in data:
+            data = data[field]
+        else:
+            return None
 
+    return data
+    #file_path = os.path.join(cwd_path, file_name)
+
+def linear_search(unordered_nums, key):
+    list_idx = []
+    count = 0
+    values_dict = dict()
+    for index, number in enumerate(unordered_nums):
+        if number == key:
+            list_idx.append(index)
+            count = count + 1
+        else:
+            continue
+    values_dict["positions"] = list_idx
+    values_dict["count"] = count
+    return values_dict
 
 def main():
-    pass
-
+    sequential_data = read_data("sequential.json", "unordered_numbers")
+    print(sequential_data)
+    print(linear_search(sequential_data, 9))
 
 if __name__ == '__main__':
     main()
